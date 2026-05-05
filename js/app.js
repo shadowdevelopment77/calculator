11963e90c09aa385d2a37dd20093451551b13f04
shadowdevelopment77 <adlydevelopment37@gmail.com>
2026-05-05 20:29:37 +0700

    calculator starting design

--- js/app.js
+++ js/app.js
@@ -1,6 +1,42 @@
+let currentNumber = "";
+let previousNumber = "";
+let operator = "";
+let shouldResetDisplay = false;
 
 
 
-function multiplication() {
-    
+const display = document.getElementById("display");
+const miniDisplay = document.getElementById("mini-display")
+
+function appendToDisplay(input){
+    if ([`+`, `-`, `*`, `/`].includes(input)) {
+        operator = input;
+        miniDisplay.value =input;
+        shouldResetDisplay = true;
+    } else{
+        if (shouldResetDisplay) {
+            display.value = input;
+            shouldResetDisplay = false
+        } else {
+            display.value += input;
+        }
+        currentNumber = display.value;
+    } 
+}
+
+function clearDisplay() {
+    display.value = "";
+    miniDisplay.value ="";
+    currentNumber = "";
+    previousNumber = "";
+    operator = "";
+    shouldResetDisplay = false;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value)
+    } catch (error) {
+        display.value = "Error"
+    }
 }
